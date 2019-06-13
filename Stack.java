98de19a838916b485d3d3812a49d62b457860da1
amujumdar1 <amujumdar@ucvts.tec.nj.us>
2019-06-13 13:53:35 -0400

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,1 +1,26 @@
+import java.util.*;
 
+public class Stack {
+  /* You must include test cases in the the @main method. Create five tests for each exercise which must be correct. Any attempts at hardcoding will result in a 0. This is how you will receive credit for this assignment. */
+  public static void main(String[] args){
+    
+  }
+  
+  /* using the given @param list, create a method that inserts the data in list into a stack and a queue, 
+  and print out the contents of each */
+  public void printStackQueues(LinkedList<Integer> list){
+     
+  }
+  
+  /* Use the below method to reverse the @param queue, and return the result. You may use loops or any 
+  inbuilt methods, but you may NOT use recursion. 
+  */
+  public Queue reverseQueue(Queue<String> queue){
+     
+  }
+  
+  /* 
+  */
+  // TODO - create a recursive method(s) to reverse a Stack. You will not receive any credit for using any loops or static variables. 
+  
+}
